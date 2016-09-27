be0d78fc8651932ebb865cafdcaa6f65759a2451
vigen-vardanyan <vigen.vardanyan@arm.synisys.com>
2016-09-27 09:01:15 +0400

    Update ContainerImpl.java

--- 2016-2/patterns/samples/behavioral/iterator/vigen.vardanyan/ContainerImpl.java
+++ 2016-2/patterns/samples/behavioral/iterator/vigen.vardanyan/ContainerImpl.java
@@ -4,7 +4,7 @@ package iterator;
  * Created by Vigen.Vardanyan on 9/27/2016.
  */
 public class ContainerImpl implements Container {
-    public String names[] = {"Rob" , "John" ,"Julie" , "Lora"};
+    public String names[] = {"Name1", "Name2", "Name3", "Name4"};
 
     public Iterator getIterator() {
         return new IteratorImpl();
@@ -15,16 +15,14 @@ public class ContainerImpl implements Container {
         int index;
 
         public boolean hasNext() {
-
-            if(index < names.length){
+            if (index < names.length) {
                 return true;
             }
             return false;
         }
 
         public Object next() {
-
-            if(this.hasNext()){
+            if (this.hasNext()) {
                 return names[index++];
             }
             return null;
